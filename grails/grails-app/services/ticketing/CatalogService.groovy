ef9798a72f748c037f71edadb8f42f98e740cb13
Hernan-dh <63477196+Hernan-dh@users.noreply.github.com>
2026-09-16 03:55:09 -0300

    fix: add @Autowired annotation to CatalogService dataSource
    Explicitly marks the dataSource field with the @Autowired annotation in CatalogService to ensure proper dependency injection by the Spring container.

--- grails/grails-app/services/ticketing/CatalogService.groovy
+++ grails/grails-app/services/ticketing/CatalogService.groovy
@@ -3,7 +3,9 @@ import javax.sql.DataSource
 import groovy.sql.Sql
 import groovy.json.JsonOutput
 import groovy.json.JsonSlurper
+import org.springframework.beans.factory.annotation.Autowired
 class CatalogService {
+    @Autowired
     DataSource dataSource
     List list() {
         new Sql(dataSource).rows('SELECT payload FROM catalog_events ORDER BY created_at, id').collect {
